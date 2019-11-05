30d3016dccc082fffb9b822a4affbb0aa6b334eb
Ibrahim Cinar <ibrahim.cinar@stud.h-da.de>
2019-11-05 01:43:28 +0100

    add some stuff from lab SS2017

---
+++ sourcen/cf-helloworld-client-1.1.0-SNAPSHOT.java
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Matthias Kovatsch - creator and main architect
+ *    Achim Kraus (Bosch Software Innovations GmbH) - add saving payload
+ ******************************************************************************/
+package org.eclipse.californium.examples;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.stream.Collectors;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.sun.jndi.toolkit.url.Uri;
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.coap.*;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.Utils;
+import sun.net.www.http.HttpClient;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class GETClient {
+
+	/*
+	 * Application entry point.
+	 * 
+	 */	
+	public static void main(String args[]) {
+		List<TimeMeasure> lst = new ArrayList<>();
+
+		try {
+			lst.add(new HttpMeasure("http:32b", new URL("http://localhost/32b.txt"), 1));
+			lst.add(new CoapMeasure("coap:32b", new URI("coap://localhost:5683/32b.txt"), 1));
+
+			lst.add(new HttpMeasure("http:2kb", new URL("http://localhost/2048b.txt"), 1));
+			lst.add(new CoapMeasure("coap:2kb", new URI("coap://localhost:5683/1024b.txt"), 2));
+
+			lst.add(new HttpMeasure("http:8kb", new URL("http://localhost/8192b.txt"), 1));
+			lst.add(new CoapMeasure("coap:8kb", new URI("coap://localhost:5683/1024b.txt"), 8));
+
+			lst.add(new HttpMeasure("http:1mb", new URL("http://localhost/1024kb.txt"), 1));
+			lst.add(new CoapMeasure("coap:1mb", new URI("coap://localhost:5683/1024b.txt"), 1024));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("#########################");
+		System.out.println("#########################");
+		System.out.println();
+
+		System.out.print("run");
+		System.out.print("\t");
+		for (TimeMeasure m: lst) {
+			System.out.print(m.getName() + "(ms)");
+			System.out.print("\t");
+		}
+		System.out.println();
+
+		for (int i = 0; i < 50; i++) {
+			System.out.print(i);
+			System.out.print("\t");
+			for (TimeMeasure m: lst) {
+				System.out.print(m.measure());
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+	}
+
+	private static interface TimeMeasure {
+		public String getName();
+		public long measure();
+	}
+
+	private static class CoapMeasure implements TimeMeasure {
+		private URI uri;
+		private CoapClient client;
+		private int loop;
+		private String name;
+
+		public CoapMeasure(String name, URI uri, int loop) {
+			this.uri = uri;
+			this.loop = loop;
+			this.name = name;
+
+			client = new CoapClient(this.uri);
+			client.useEarlyNegotiation(1024);
+			client.get();
+		}
+
+		public long measure() {
+			long start = System.currentTimeMillis();
+			boolean isSuccess = true;
+			CoapResponse response = null;
+
+			for (int i = 0; i < loop; i++) {
+				String content = "";
+				response = client.get();
+				if (response != null) {
+					content += response.getResponseText();
+				}
+				isSuccess &= response != null && response.isSuccess();
+			}
+			long duration = System.currentTimeMillis() - start;
+			if (isSuccess)
+				return duration;
+			else
+				return -1;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+	}
+
+	private static class HttpMeasure implements TimeMeasure {
+		private URL url;
+		private int loop;
+		private String name;
+
+		public HttpMeasure(String name, URL url, int loop) {
+			this.url = url;
+			this.loop = loop;
+			this.name = name;
+		}
+
+		public long measure() {
+			try {
+				long start = System.currentTimeMillis();
+				for (int i = 0; i < loop; i++) {
+					HttpURLConnection con = (HttpURLConnection) url.openConnection();
+					InputStream is = con.getInputStream();
+					String content = "";
+					try (BufferedReader buffer = new BufferedReader(new InputStreamReader(is))) {
+						content = buffer.lines().collect(Collectors.joining("\n"));
+					}
+				}
+				long duration = System.currentTimeMillis() - start;
+
+				return duration;
+			}
+			catch (Exception e)
+			{
+				//e.printStackTrace();
+				return -1;
+			}
+		}
+
+		public String getName() {
+			return this.name;
+		}
+	}
+
+}
---
+++ sourcen/cf-helloworld-server-1.1.0-SNAPSHOT.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ * 
+ * Contributors:
+ *    Matthias Kovatsch - creator and main architect
+ *    Kai Hudalla (Bosch Software Innovations GmbH) - add endpoints for all IP addresses
+ ******************************************************************************/
+package org.eclipse.californium.examples;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.Response;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.EndpointManager;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+
+public class HelloWorldServer extends CoapServer {
+
+	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
+    /*
+     * Application entry point.
+     */
+    public static void main(String[] args) {
+        
+        try {
+
+            // create server
+            HelloWorldServer server = new HelloWorldServer();
+            // add endpoints on all IP addresses
+            server.addEndpoints();
+            server.start();
+
+        } catch (SocketException e) {
+            System.err.println("Failed to initialize server: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Add individual endpoints listening on default CoAP port on all IPv4 addresses of all network interfaces.
+     */
+    private void addEndpoints() {
+    	for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
+    		// only binds to IPv4 addresses and localhost
+			if (addr instanceof Inet4Address || addr.isLoopbackAddress()) {
+				InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
+				addEndpoint(new CoapEndpoint(bindToAddress));
+			}
+		}
+    }
+
+    /*
+     * Constructor for a new Hello-World server. Here, the resources
+     * of the server are initialized.
+     */
+    public HelloWorldServer() throws SocketException {
+        Path dir = Paths.get("content");
+        if (Files.exists(dir)) {
+            try {
+                Files.list(dir).forEach(file -> add(new SimpleFileRessource(file.toAbsolutePath())));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Files.createDirectory(dir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*
+     * Definition of the Hello-World Resource
+     */
+    class HelloWorldResource extends CoapResource {
+        
+        public HelloWorldResource() {
+            
+            // set resource identifier
+            super("helloWorld");
+            
+            // set display name
+            getAttributes().setTitle("Hello-World Resource");
+        }
+
+        @Override
+        public void handleGET(CoapExchange exchange) {
+            
+            // respond to the request
+            exchange.respond("Hello World!");
+        }
+    }
+
+    class SimpleJSONRessource extends CoapResource {
+
+        public  SimpleJSONRessource() {
+            super("simpleJSON");
+            getAttributes().setTitle(this.getName());
+        }
+
+        public void handleGET(CoapExchange exchange) {
+            exchange.respond("{name: \"simpleJSON\", status: \"up\", size: \"51 Byte\"}");
+        }
+    }
+
+    class SimpleFileRessource extends CoapResource{
+
+        byte[] payload;
+        int mediaType;
+
+        public SimpleFileRessource() {
+            super("emptyFile");
+            getAttributes().setTitle(this.getName());
+        }
+
+        public SimpleFileRessource(Path path) {
+            super(path.getFileName().toString());
+            try {
+                this.payload = Files.readAllBytes(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String[] tmp = path.toString().split("\\.");
+            switch (tmp[tmp.length -1]){
+                case "jpg":
+                case "jpeg":
+                    mediaType = MediaTypeRegistry.IMAGE_JPEG;
+                    break;
+                case "png":
+                    mediaType = MediaTypeRegistry.IMAGE_PNG;
+                    break;
+                default:
+                    mediaType = MediaTypeRegistry.TEXT_PLAIN;
+            }
+            getAttributes().setTitle(this.getName());
+        }
+
+        public void handleGET(CoapExchange exchange) {
+            if (payload == null) {
+                exchange.respond("no content");
+            }
+            else {
+                Response resp = new Response(CoAP.ResponseCode.CONTENT);
+                resp.getOptions().setContentFormat(this.mediaType);
+                resp.setPayload(this.payload);
+                exchange.respond(resp);
+            }
+        }
+    }
+}
